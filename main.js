afe30c6cef62ad9df9975a27a1da12f0405fea3b
Phil Hughes <me@iamphill.com>
2019-08-05 09:58:12 +0100

    Use menubar package
    Use menubar package to create the tray bar app

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const http = require('http')
-const { platform } = require('os');
+const path = require('path');
 const { Nuxt, Builder } = require('nuxt')
-const { resolve: resolvePath } = require('app-root-path');
-const { getWindow, toggleWindow } = require('./main/window');
+const { menubar } = require('menubar');
+
 let config = require('./nuxt.config.js')
-// Check if Windows or Mac
 config.rootDir = __dirname // for electron-builder
+
 // Init Nuxt.js
 const nuxt = new Nuxt(config)
 const builder = new Builder(nuxt)
 const server = http.createServer(nuxt.render)
+
 // Build only in dev mode
 if (config.dev) {
   builder.build().catch(err => {
@@ -22,62 +23,41 @@ server.listen()
 const _NUXT_URL_ = `http://localhost:${server.address().port}`
 console.log(`Nuxt working on ${_NUXT_URL_}`)
 
-/*
-** Electron
-*/
-let win = null // Current window
-const electron = require('electron')
-const path = require('path')
-const app = electron.app
-let tray = null
-app.on('ready', async () => {
-  console.log('ready')
-  try {
-    const iconName =
-      process.platform === 'win32'
-        ? 'iconWhite'
-        : process.platform === 'linux'
-        ? 'iconWhite'
-        : 'iconTemplate'
-    tray = new electron.Tray(resolvePath(`./static/${iconName}.png`))
-  } catch (error) {
-    console.log('error', error)
-    return
+const mb = menubar({
+  index: _NUXT_URL_,
+  icon: path.join(__dirname, 'static/iconTemplate.png'),
+  browserWindow: {
+    width: 330,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   }
+});
 
-  // Opening the context menu after login should work
-  global.tray = tray
-
-  // Generate the browser window
-  const win = getWindow(tray)
-  const toggleActivity = () => toggleWindow(tray, win);
+mb.on('after-create-window', () => {
+  mb.window.setAlwaysOnTop(true)
 
   if (config.dev) {
     // Install vue dev tool and open chrome dev tools
     const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer')
     installExtension(VUEJS_DEVTOOLS.id).then(name => {
-      console.log(`Added Extension:  ${name}`)
-      win.webContents.openDevTools()
-    }).catch(err => console.log('An error occurred: ', err))
+      console.log(`Added Extension:  ${name}`);
+      mb.window.webContents.openDevTools();
+    }).catch(err => console.log('An error occurred: ', err));
     // Wait for nuxt to build
     const pollServer = () => {
       http.get(_NUXT_URL_, (res) => {
-        if (res.statusCode === 200) { win.loadURL(_NUXT_URL_) } else { setTimeout(pollServer, 300) }
-      }).on('error', pollServer)
+        if (res.statusCode === 200) {
+          mb.window.loadURL(_NUXT_URL_);
+        } else {
+          setTimeout(pollServer, 300);
+        }
+      }).on('error', pollServer);
     }
-    pollServer()
-  } else { return win.loadURL(_NUXT_URL_) }
+    pollServer();
+  } else {
+    mb.window.loadURL(_NUXT_URL_);
+  }
+});
 
-  tray.on('click', toggleActivity);
-  tray.on('double-click', toggleActivity);
-
-  tray.on('right-click', async event => {
-    if (win.isVisible()) {
-      win.hide();
-      return;
-    }
-    event.preventDefault();
-  })
-})
-
-process.on('uncaughtException', error => console.log(error))
+mb.app.on('window-all-closed', () => mb.app.quit())
