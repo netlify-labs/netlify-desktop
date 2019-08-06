a0fbba783814887a57a847b62d80f876930842e3
Phil Hughes <me@iamphill.com>
2019-08-06 16:20:41 +0100

    Style site deploy list
    Closes #5

---
+++ components/ui/Badge.vue
@@ -0,0 +1,50 @@
+<template>
+  <span :class="variant" class="badge"><slot></slot></span>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.badge {
+  align-self: center;
+  background: rgba(14,30,37,.05);
+  border-radius: 3px;
+  color: #2d3b41;
+  display: inline-block;
+  flex: 0 0 auto;
+  font-size: 11px;
+  font-weight: 700;
+  line-height: 16px;
+  margin-left: 8px;
+  padding: 2px 4px;
+  position: relative;
+  text-transform: uppercase;
+  top: -1px;
+  vertical-align: baseline;
+  white-space: nowrap;
+}
+
+.badge.success {
+  background-color: rgba(0,173,159,.06);
+  color: #00ad9e;
+}
+
+.badge.error {
+  background-color: rgba(250,56,69,.06);
+  color: #f93845;
+}
+
+.badge.in-progress {
+  background-color: rgba(219,168,0,.06);
+  color: #dba800;
+}
+</style>
--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-const http = require('http')
+const http = require('http');
 const path = require('path');
-const { Nuxt, Builder } = require('nuxt')
+const { Nuxt, Builder } = require('nuxt');
 const { menubar } = require('menubar');
 
-let config = require('./nuxt.config.js')
-config.rootDir = __dirname // for electron-builder
+let config = require('./nuxt.config.js');
+config.rootDir = __dirname; // for electron-builder
 
 // Init Nuxt.js
-const nuxt = new Nuxt(config)
-const builder = new Builder(nuxt)
-const server = http.createServer(nuxt.render)
+const nuxt = new Nuxt(config);
+const builder = new Builder(nuxt);
+const server = http.createServer(nuxt.render);
 
 // Build only in dev mode
 if (config.dev) {
   builder.build().catch(err => {
-    console.error(err) // eslint-disable-line no-console
-    process.exit(1)
-  })
+    console.error(err); // eslint-disable-line no-console
+    process.exit(1);
+  });
 }
 // Listen the server
-server.listen()
-const _NUXT_URL_ = `http://localhost:${server.address().port}`
-console.log(`Nuxt working on ${_NUXT_URL_}`)
+server.listen();
+const _NUXT_URL_ = `http://localhost:${server.address().port}`;
+console.log(`Nuxt working on ${_NUXT_URL_}`);
 
 const mb = menubar({
   index: _NUXT_URL_,
@@ -30,34 +30,55 @@ const mb = menubar({
     width: 330,
     webPreferences: {
       nodeIntegration: true,
-    }
-  }
+    },
+  },
 });
 
 mb.on('after-create-window', () => {
-  mb.window.setAlwaysOnTop(true)
+  mb.window.setAlwaysOnTop(true);
+
+  mb.window.webContents.on('will-navigate', (event, url) => {
+    if (!url.startsWith('http://localhost')) {
+      event.preventDefault();
+      shell.openExternal(url);
+    }
+  });
+
+  win.webContents.on('will-navigate', (event, url) => {
+    if (!url.startsWith('http://localhost')) {
+      event.preventDefault();
+      shell.openExternal(url);
+    }
+  });
 
   if (config.dev) {
     // Install vue dev tool and open chrome dev tools
-    const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer')
-    installExtension(VUEJS_DEVTOOLS.id).then(name => {
-      console.log(`Added Extension:  ${name}`);
-      mb.window.webContents.openDevTools();
-    }).catch(err => console.log('An error occurred: ', err));
+    const {
+      default: installExtension,
+      VUEJS_DEVTOOLS,
+    } = require('electron-devtools-installer');
+    installExtension(VUEJS_DEVTOOLS.id)
+      .then(name => {
+        console.log(`Added Extension:  ${name}`);
+        mb.window.webContents.openDevTools();
+      })
+      .catch(err => console.log('An error occurred: ', err));
     // Wait for nuxt to build
     const pollServer = () => {
-      http.get(_NUXT_URL_, (res) => {
-        if (res.statusCode === 200) {
-          mb.window.loadURL(_NUXT_URL_);
-        } else {
-          setTimeout(pollServer, 300);
-        }
-      }).on('error', pollServer);
-    }
+      http
+        .get(_NUXT_URL_, res => {
+          if (res.statusCode === 200) {
+            mb.window.loadURL(_NUXT_URL_);
+          } else {
+            setTimeout(pollServer, 300);
+          }
+        })
+        .on('error', pollServer);
+    };
     pollServer();
   } else {
     mb.window.loadURL(_NUXT_URL_);
   }
 });
 
-mb.app.on('window-all-closed', () => mb.app.quit())
+mb.app.on('window-all-closed', () => mb.app.quit());
